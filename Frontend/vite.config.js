60d4a384bb60316f89f84fa5f8a197a0546ef3f3
orionpax12 <aman1312sharma@gmail.com>
2024-12-09 15:09:31 +0530

    setup

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -5,6 +5,9 @@ import { defineConfig } from "vite"
 export default defineConfig({
   plugins: [react()],
   base: '/',
+  build:{
+    outDir: 'Frontend/dist'
+  },
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
